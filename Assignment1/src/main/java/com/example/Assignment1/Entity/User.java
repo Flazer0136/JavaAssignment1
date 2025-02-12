f5b44fc0f8ec2e999e7d95130e2f481cfb4e4ffc
Flazer <146836746+Flazer0136@users.noreply.github.com>
2025-02-11 19:29:12 -0700

    Final version

---
+++ Assignment1/src/main/java/com/example/Assignment1/Entity/User.java
@@ -0,0 +1,76 @@
+package com.example.Assignment1.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userID;
+    private String username;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String passwordhash;
+    private String salt;
+
+    // Getters and Setters
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPasswordhash() {
+        return passwordhash;
+    }
+
+    public void setPasswordhash(String passwordhash) {
+        this.passwordhash = passwordhash;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+}
---
+++ Assignment1/src/main/java/com/example/Assignment1/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.Assignment1.Repository;
+
+import com.example.Assignment1.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
